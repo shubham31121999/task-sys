6ea118f4688ba257e6baac92369ff2b989902847
Shubham Gajanan Uparkar <65241730+shubham31121999@users.noreply.github.com>
2025-08-16 00:41:33 +0530

    Done

--- backend/backendside/backendside/settings.py
+++ backend/backendside/backendside/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-6j2sx+=lz51oz(ee6a_)z(b7k0$x54i1$w+rxtpsunl$lb$3t)"
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.onrender.com']  # allows Render domain
 
 
 # Application definition
